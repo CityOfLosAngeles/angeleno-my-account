189fcf89fc690b1c82cd5e07303f77b7d79119ef
Cristian Hernandez <cristian.b.hernandez@lacity.org>
2023-09-06 13:50:29 -0700

    Initial unit test

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -119,8 +119,8 @@ class _MyHomePageState extends State<MyHomePage> {
           ) : Container(
                color: footerBlue,
                padding: const EdgeInsets.all(16.0),
-               child: const Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
+               child: const Wrap(
+                 alignment: WrapAlignment.center,
                  children: [
                    Text('© Copyright 2023 City of Los Angeles. '
                        'All rights reserved. Disclaimer | Privacy Policy',
