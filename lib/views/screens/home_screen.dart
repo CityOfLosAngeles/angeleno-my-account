b89fc23e3cc56a08736190379fdf71e1c2225f31
Cristian Hernandez <cristian.b.hernandez@lacity.org>
2023-11-01 11:54:52 -0700

    Center and Max Width

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -157,19 +157,22 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-          body: Container(
+          body: Center(
+            child: Container(
               transformAlignment: Alignment.center,
-
+              width: double.infinity,
+              constraints: const BoxConstraints(maxWidth: 1280),
               child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: screens[_selectedIndex]))
-                  ],
-                ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: screens[_selectedIndex]))
+                ],
               ),
+            )
+          ),
           bottomNavigationBar: Container(
             padding: const EdgeInsets.all(16.0),
             child: const Wrap(
