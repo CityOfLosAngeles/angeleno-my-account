79d39d08caa6a70310acd571afca8da83a5fba0b
Paz-X <Alfredo.Zanchez@lacity.org>
2023-12-11 11:20:38 -0800

    Adding Address Validation

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -32,19 +32,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
     // Only submit patch if data has been updated
     if (!(user == userProvider.cleanUser)) {
       UserApi().updateUser(user).then((final response) {
-          final success = response == HttpStatus.ok;
-          overlayProvider.hideLoading();
-          ScaffoldMessenger.of(context).showSnackBar( SnackBar(
-              behavior: SnackBarBehavior.floating,
-              width: 280.0,
-              content: Text(success ? 'User updated' : 'User update failed'),
-              action: success ? null : SnackBarAction(
-                  label: 'Retry',
-                  onPressed: () {
-                    updateUser();
-                  }
-              )
-          ));
+        final success = response == HttpStatus.ok;
+        overlayProvider.hideLoading();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            behavior: SnackBarBehavior.floating,
+            width: 280.0,
+            content: Text(success ? 'User updated' : 'User update failed'),
+            action: success
+                ? null
+                : SnackBarAction(
+                    label: 'Retry',
+                    onPressed: () {
+                      updateUser();
+                    })));
       });
     }
   }
@@ -72,111 +72,109 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     },
                     autovalidateMode: AutovalidateMode.disabled,
                     child: Column(
-                        children: [
-                          const SizedBox(height: 10.0),
-                          Row(mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const SizedBox(height: 10.0),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
                             children: [
                               ElevatedButton(
-                              onPressed: () {
-                                if (userProvider.isEditing) {
-                                  updateUser();
-                                }
-                                setState(() {
-                                  userProvider.toggleEditing();
-                                });
-                              },
-                              child: Text(
-                                  userProvider.isEditing ? 'Save' : 'Edit'
-                              ),
-                            )
-                          ]),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'First Name',
-                                border: OutlineInputBorder()),
-                            initialValue: user.firstName,
-                            keyboardType: TextInputType.name,
-                            onChanged: (final val) {
-                              user.firstName = val;
-                            },
-                          ),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'Last Name',
-                                border: OutlineInputBorder()),
-                            initialValue: user.lastName,
-                            keyboardType: TextInputType.name,
-                            onChanged: (final val) {
-                              user.lastName = val;
-                            },
-                          ),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'Zip', border: OutlineInputBorder()),
-                            initialValue: user.zip,
-                            onChanged: (final val) {
-                              user.zip = val;
-                            },
-                            keyboardType: TextInputType.number,
-                          ),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'Address',
-                                border: OutlineInputBorder()),
-                            keyboardType: TextInputType.streetAddress,
-                            initialValue: user.address,
-                            onChanged: (final val) {
-                              user.address = val;
-                            },
-                          ),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'City',
-                                border: OutlineInputBorder()),
-                            keyboardType: TextInputType.streetAddress,
-                            initialValue: user.city,
-                            onChanged: (final val) {
-                              user.city = val;
-                            },
-                          ),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'State',
-                                border: OutlineInputBorder()),
-                            keyboardType: TextInputType.streetAddress,
-                            initialValue: user.state,
-                            onChanged: (final val) {
-                              user.state = val;
-                            },
-                          ),
-                          const SizedBox(height: 25.0),
-                          TextFormField(
-                            enabled: userProvider.isEditing,
-                            decoration: const InputDecoration(
-                                labelText: 'Mobile',
-                                border: OutlineInputBorder()),
-                            initialValue: user.phone,
-                            onChanged: (final val) {
-                              user.phone = val;
-                            },
-                          ),
-                        ],
-                    )
-                )
-            )
-        )
+                                onPressed: () {
+                                  if (userProvider.isEditing) {
+                                    updateUser();
+                                  }
+                                  setState(() {
+                                    userProvider.toggleEditing();
+                                  });
+                                },
+                                child: Text(
+                                    userProvider.isEditing ? 'Save' : 'Edit'),
+                              )
+                            ]),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          enabled: userProvider.isEditing,
+                          decoration: const InputDecoration(
+                              labelText: 'First Name',
+                              border: OutlineInputBorder()),
+                          initialValue: user.firstName,
+                          keyboardType: TextInputType.name,
+                          onChanged: (final val) {
+                            user.firstName = val;
+                          },
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          enabled: userProvider.isEditing,
+                          decoration: const InputDecoration(
+                              labelText: 'Last Name',
+                              border: OutlineInputBorder()),
+                          initialValue: user.lastName,
+                          keyboardType: TextInputType.name,
+                          onChanged: (final val) {
+                            user.lastName = val;
+                          },
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          enabled: userProvider.isEditing,
+                          decoration: const InputDecoration(
+                              labelText: 'Address',
+                              border: OutlineInputBorder()),
+                          keyboardType: TextInputType.streetAddress,
+                          initialValue: user.address,
+                          onChanged: (final val) {
+                            user.address = val;
+                          },
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          enabled: false,
+                          //enabled: userProvider.isEditing,
+                          decoration: const InputDecoration(
+                              labelText: 'City', border: OutlineInputBorder()),
+                          keyboardType: TextInputType.streetAddress,
+                          initialValue: user.city,
+                          onChanged: (final val) {
+                            user.city = val;
+                          },
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          enabled: false,
+                          //enabled: userProvider.isEditing,
+                          decoration: const InputDecoration(
+                              labelText: 'State', border: OutlineInputBorder()),
+                          keyboardType: TextInputType.streetAddress,
+                          initialValue: user.state,
+                          onChanged: (final val) {
+                            user.state = val;
+                          },
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          //enabled: userProvider.isEditing,
+                          enabled: false,
+                          decoration: const InputDecoration(
+                              labelText: 'Zip', border: OutlineInputBorder()),
+                          initialValue: user.zip,
+                          onChanged: (final val) {
+                            user.zip = val;
+                          },
+                          keyboardType: TextInputType.number,
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          enabled: userProvider.isEditing,
+                          decoration: const InputDecoration(
+                              labelText: 'Mobile',
+                              border: OutlineInputBorder()),
+                          initialValue: user.phone,
+                          onChanged: (final val) {
+                            user.phone = val;
+                          },
+                        ),
+                      ],
+                    ))))
       ],
     );
   }
