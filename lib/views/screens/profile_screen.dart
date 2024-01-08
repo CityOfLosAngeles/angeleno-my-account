6484b9829341346ac7ae8229364849ceb4bc609f
Paz-X <Alfredo.Zanchez@lacity.org>
2024-01-08 12:33:34 -0800

    Added loadAddress upon loading from Auth0

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -46,7 +46,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
     super.initState();
     sessionToken = Uuid().v4(); //Create token for the session
     apiClient = PlaceAPI(sessionToken);
+
     //loadAddress();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    overlayProvider = context.watch<OverlayProvider>();
+    userProvider = context.watch<UserProvider>();
+    loadUser();
   }
 
   @override
@@ -82,6 +92,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   void loadAddress() {
+    print('Loading Address');
     try {
       usrAddressTextController.text = user.address!;
       usrCityTextController.text = user.city!;
@@ -89,6 +100,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
       usrZipTextController.text = user.zip!;
     } catch (e) {
       print('Cannot load address $e');
+    }
+  }
+
+  loadUser() {
+    if (userProvider.user == null) {
+      //return const LinearProgressIndicator();
+      print('user is null');
+    } else {
+      user = userProvider.user!;
+      if (!autoFilled) {
+        loadAddress();
+      }
     }
   }
 
@@ -136,172 +159,171 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(final BuildContext context) {
-    overlayProvider = context.watch<OverlayProvider>();
-    userProvider = context.watch<UserProvider>();
-
     if (userProvider.user == null) {
       return const LinearProgressIndicator();
     } else {
-      user = userProvider.user!;
-      loadAddress();
-    }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-            child: SingleChildScrollView(
-                child: Form(
-                    key: formKey,
-                    onChanged: () {
-                      formKey.currentState!.save();
-                    },
-                    autovalidateMode: AutovalidateMode.disabled,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 10.0),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  if (userProvider.isEditing) {
-                                    updateUser();
-                                  }
-                                  setState(() {
-                                    userProvider.toggleEditing();
-                                  });
-                                },
-                                child: Text(
-                                    userProvider.isEditing ? 'Save' : 'Edit'),
-                              )
-                            ]),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: userProvider.isEditing,
-                          decoration: const InputDecoration(
-                              labelText: 'First Name',
-                              border: OutlineInputBorder()),
-                          initialValue: user.firstName,
-                          keyboardType: TextInputType.name,
-                          onChanged: (final val) {
-                            user.firstName = val;
-                          },
-                        ),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: userProvider.isEditing,
-                          decoration: const InputDecoration(
-                              labelText: 'Last Name',
-                              border: OutlineInputBorder()),
-                          initialValue: user.lastName,
-                          keyboardType: TextInputType.name,
-                          onChanged: (final val) {
-                            user.lastName = val;
-                          },
-                        ),
-                        const SizedBox(height: 25.0),
-                        TypeAheadField<AutofillSuggestion>(
-                            controller: usrAddressTextController,
-                            // suggestionsCallback: (search) => CityService.of(context).find(search),
-                            debounceDuration: const Duration(milliseconds: 555),
-                            hideOnEmpty: true,
-                            suggestionsCallback: (search) async {
-                              try {
-                                if (search.isEmpty || autoFilled) {
-                                  return [];
-                                } else {
-                                  final cities = await fetchSuggestions(search);
-                                  return cities;
-                                }
-                              } catch (error) {
-                                // Handle errors here
-                                print(error);
-                                return []; // Return an empty list in case of errors
-                              }
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child: SingleChildScrollView(
+                  child: Form(
+                      key: formKey,
+                      onChanged: () {
+                        formKey.currentState!.save();
+                      },
+                      autovalidateMode: AutovalidateMode.disabled,
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 10.0),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    if (userProvider.isEditing) {
+                                      updateUser();
+                                    }
+                                    setState(() {
+                                      userProvider.toggleEditing();
+                                    });
+                                  },
+                                  child: Text(
+                                      userProvider.isEditing ? 'Save' : 'Edit'),
+                                )
+                              ]),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: userProvider.isEditing,
+                            decoration: const InputDecoration(
+                                labelText: 'First Name',
+                                border: OutlineInputBorder()),
+                            initialValue: user.firstName,
+                            keyboardType: TextInputType.name,
+                            onChanged: (final val) {
+                              user.firstName = val;
                             },
-                            builder: (context, controller, focusNode) =>
-                                TextField(
-                                  controller: controller,
-                                  focusNode: focusNode,
-                                  //autofocus: false,
-                                  decoration: InputDecoration(
-                                      suffixIcon: IconButton(
-                                        icon: Icon(Icons.clear),
-                                        onPressed: () {
-                                          usrAddressTextController.clear();
-                                        },
-                                      ),
-                                      labelText: 'Address',
-                                      border: OutlineInputBorder()),
-                                ),
-                            itemBuilder: (context, address) => ListTile(
-                                  title: Text(address.description!),
-                                  subtitle: Text(address.streetAddress!),
-                                ),
-                            onSelected: onSuggestionSelected),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: userProvider.isEditing,
-                          decoration: const InputDecoration(
-                              labelText: 'Address 2',
-                              border: OutlineInputBorder()),
-                          keyboardType: TextInputType.streetAddress,
-                          initialValue: user.address2,
-                          onChanged: (final val) {
-                            user.address2 = val;
-                          },
-                        ),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: false,
-                          controller: usrCityTextController,
-                          decoration: const InputDecoration(
-                              labelText: 'City', border: OutlineInputBorder()),
-                          keyboardType: TextInputType.streetAddress,
-                          //initialValue: user.city,
-                          onChanged: (final val) {
-                            user.city = val;
-                          },
-                        ),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: false,
-                          controller: usrZipTextController,
-                          decoration: const InputDecoration(
-                              labelText: 'Zip', border: OutlineInputBorder()),
-                          //initialValue: user.zip,
-                          onChanged: (final val) {
-                            user.zip = val;
-                          },
-                          keyboardType: TextInputType.number,
-                        ),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: false,
-                          controller: usrStateTextController,
-                          decoration: const InputDecoration(
-                              labelText: 'State', border: OutlineInputBorder()),
-                          keyboardType: TextInputType.streetAddress,
-                          //initialValue: user.state,
-                          onChanged: (final val) {
-                            user.state = val;
-                          },
-                        ),
-                        const SizedBox(height: 25.0),
-                        TextFormField(
-                          enabled: userProvider.isEditing,
-                          decoration: const InputDecoration(
-                              labelText: 'Mobile',
-                              border: OutlineInputBorder()),
-                          initialValue: user.phone,
-                          onChanged: (final val) {
-                            user.phone = val;
-                          },
-                        ),
-                      ],
-                    ))))
-      ],
-    );
+                          ),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: userProvider.isEditing,
+                            decoration: const InputDecoration(
+                                labelText: 'Last Name',
+                                border: OutlineInputBorder()),
+                            initialValue: user.lastName,
+                            keyboardType: TextInputType.name,
+                            onChanged: (final val) {
+                              user.lastName = val;
+                            },
+                          ),
+                          const SizedBox(height: 25.0),
+                          TypeAheadField<AutofillSuggestion>(
+                              controller: usrAddressTextController,
+                              // suggestionsCallback: (search) => CityService.of(context).find(search),
+                              debounceDuration:
+                                  const Duration(milliseconds: 555),
+                              hideOnEmpty: true,
+                              hideOnUnfocus: true,
+                              suggestionsCallback: (search) async {
+                                try {
+                                  if (search.isEmpty || autoFilled) {
+                                    return [];
+                                  } else {
+                                    final cities =
+                                        await fetchSuggestions(search);
+                                    return cities;
+                                  }
+                                } catch (error) {
+                                  // Handle errors here
+                                  print(error);
+                                  return []; // Return an empty list in case of errors
+                                }
+                              },
+                              builder: (context, controller, focusNode) =>
+                                  TextField(
+                                    controller: controller,
+                                    focusNode: focusNode,
+                                    //autofocus: false,
+                                    decoration: InputDecoration(
+                                        suffixIcon: IconButton(
+                                          icon: Icon(Icons.clear),
+                                          onPressed: () {
+                                            usrAddressTextController.clear();
+                                          },
+                                        ),
+                                        labelText: 'Address',
+                                        border: OutlineInputBorder()),
+                                  ),
+                              itemBuilder: (context, address) => ListTile(
+                                    title: Text(address.description!),
+                                    subtitle: Text(address.streetAddress!),
+                                  ),
+                              onSelected: onSuggestionSelected),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: userProvider.isEditing,
+                            decoration: const InputDecoration(
+                                labelText: 'Address 2',
+                                border: OutlineInputBorder()),
+                            keyboardType: TextInputType.streetAddress,
+                            initialValue: user.address2,
+                            onChanged: (final val) {
+                              user.address2 = val;
+                            },
+                          ),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: false,
+                            controller: usrCityTextController,
+                            decoration: const InputDecoration(
+                                labelText: 'City',
+                                border: OutlineInputBorder()),
+                            keyboardType: TextInputType.streetAddress,
+                            //initialValue: user.city,
+                            onChanged: (final val) {
+                              user.city = val;
+                            },
+                          ),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: false,
+                            controller: usrZipTextController,
+                            decoration: const InputDecoration(
+                                labelText: 'Zip', border: OutlineInputBorder()),
+                            //initialValue: user.zip,
+                            onChanged: (final val) {
+                              user.zip = val;
+                            },
+                            keyboardType: TextInputType.number,
+                          ),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: false,
+                            controller: usrStateTextController,
+                            decoration: const InputDecoration(
+                                labelText: 'State',
+                                border: OutlineInputBorder()),
+                            keyboardType: TextInputType.streetAddress,
+                            //initialValue: user.state,
+                            onChanged: (final val) {
+                              user.state = val;
+                            },
+                          ),
+                          const SizedBox(height: 25.0),
+                          TextFormField(
+                            enabled: userProvider.isEditing,
+                            decoration: const InputDecoration(
+                                labelText: 'Mobile',
+                                border: OutlineInputBorder()),
+                            initialValue: user.phone,
+                            onChanged: (final val) {
+                              user.phone = val;
+                            },
+                          ),
+                        ],
+                      ))))
+        ],
+      );
+    }
   }
 }
