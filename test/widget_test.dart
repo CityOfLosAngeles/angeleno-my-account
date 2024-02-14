97680075682789a3c3aebc7b28ce2dc095ff67f4
Cristian Hernandez <cristian.b.hernandez@lacity.org>
2024-02-13 16:24:12 -0800

    Update MyApp test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:angeleno_project/controllers/overlay_provider.dart';
 import 'package:angeleno_project/controllers/user_provider.dart';
+import 'package:angeleno_project/views/screens/home_screen.dart';
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -53,7 +54,7 @@ void main() {
       )
     );
 
-    expect(find.byType(MyApp), findsOneWidget);
+    expect(find.byType(MyHomePage), findsOneWidget);
     expect(find.text('Angeleno Account'), findsOneWidget);
   });
 
