511191d2e6a3c84660a5373d2fbfefc1aaa9ccd9
Cristian Hernandez <cristian.b.hernandez@lacity.org>
2024-03-21 15:18:10 -0700

    Opens and closes dialog

--- test/advanced_test.dart
+++ test/advanced_test.dart
@@ -1,6 +1,7 @@
 import 'package:angeleno_project/controllers/api_implementation.dart';
 import 'package:angeleno_project/controllers/user_provider.dart';
 import 'package:angeleno_project/models/api_response.dart';
+import 'package:angeleno_project/views/dialogs/mobile.dart';
 import 'package:angeleno_project/views/screens/advanced_security_screen.dart';
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,8 @@ import 'mocks/advanced_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<UserApi>()])
 void main() {
+
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   late MockUserApi mockUserApi;
 
@@ -133,34 +136,10 @@ void main() {
     await tester.tap(find.byKey( const Key('enableSMS')));
     await tester.pumpAndSettle();
 
-    // -------------------------------------
-
-    final inputTextFieldFinder = find.byKey(const Key('intl_text_input_key'));
-
-    await tester.tap(inputTextFieldFinder);
-   
-    await tester.enterText(find.byKey(const Key('phoneField')), '8');
-
+    expect(find.byType(MobileDialog), findsOneWidget);
+    await tester.tap(find.byIcon(Icons.close));
     await tester.pumpAndSettle();
-    
-
-    // -------------------------------------
-
-
-    await tester.tap(find.widgetWithText(TextButton, 'Continue'));
-    await tester.pumpAndSettle();
-
-    await tester.enterText(find.byKey(const Key('passwordField')), 'myPassword');
-    await tester.tap(find.widgetWithText(TextButton, 'Continue'));
-    await tester.pumpAndSettle();
-
-    await tester.enterText(find.byKey(const Key('phoneCode')), '483234');
-    await tester.tap(find.widgetWithText(TextButton, 'Continue'));
-    await tester.pumpAndSettle();
-
-    expect(find.byType(SnackBar), findsOneWidget);
-    expect(find.byKey(const Key('disableSMS')), findsOneWidget);
-
+    expect(find.byType(MobileDialog), findsNothing);
 
   });
 }
