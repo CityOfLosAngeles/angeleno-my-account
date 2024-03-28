32d994a876750bf086a3babc459f9def085c40ec
Cristian Hernandez <cristian.b.hernandez@lacity.org>
2024-03-28 11:46:57 -0700

    remove unused imports

--- test/auth0_api_test.dart
+++ test/auth0_api_test.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'dart:io';
-import 'package:angeleno_project/controllers/api.dart';
 import 'package:angeleno_project/models/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
@@ -19,8 +17,7 @@ void main() {
   });
 
   test('updateUser returns status code 200 on success', () async {
-    final client = MockClient();
-    final updateUserUrl = Uri.parse('/auth0/updateUser');
+
     final user = User(
       userId: '123456',
       email: 'test@example.com',
