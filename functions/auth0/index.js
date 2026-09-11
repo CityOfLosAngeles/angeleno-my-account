351b91036cc5751401a8b4e091437f5e809a27df
Cristian Hernandez <cristian.b.hernandez@lacity.org>
2026-09-11 12:14:10 -0700

    Remove Service Account usage

--- functions/auth0/index.js
+++ functions/auth0/index.js
@@ -5,69 +5,54 @@ import jwt from 'jsonwebtoken';
 import jwksClient from 'jwks-rsa';
 import { setGlobalOptions } from 'firebase-functions/v2';
 import { auth0Domain } from './utils/constants.js';
-
-import {
-  updateUser,
-  updatePassword,
-  enrollMFA,
-  confirmMFA,
-  authMethods,
-  unenrollMFA,
-  removeConnection,
-  challengeMfa,
-  requestMFAToken
-} from './api/auth0.js';
+import * as auth0 from './api/auth0.js';
 
 admin.initializeApp();
 setGlobalOptions({
   region: 'us-west1'
 })
-const app = express();
 
+const app = express();
 const client = jwksClient({
   jwksUri: `https://${auth0Domain}/.well-known/jwks.json`
 });
 
-function getKey(header, callback){
-  client.getSigningKey(header.kid, function(err, key) {
-    var signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
-  });
-}
-
-app.use((req, res, next) => {
-
-  const accessTokenHeader = req.headers['X-ACCESS-TOKEN'] || req.headers['x-access-token'];
+const verifyToken = (req, res, next) => {
+  const token = req.headers['X-ACCESS-TOKEN'] || req.headers['x-access-token'];
   const userId = req.body.userId || req.query.userId;
 
-  if (!accessTokenHeader) {
+  if (!token) {
     return res.status(401).send('Unauthorized: No token provided');
   }
 
-  jwt.verify(accessTokenHeader, getKey, (err, decoded) => {
+  jwt.verify(token, (header, callback) => {
+    client.getSigningKey(header.kid, (err, key) => {
+      callback(err, key?.publicKey || key?.rsaPublicKey);
+    });
+  }, (err, decoded) => {
     if (err) {
       console.error('Token verification failed:', err);
       return res.status(401).send('Unauthorized: Invalid token');
     }
-
     if (decoded.sub !== userId) {
       return res.status(401).send('Unauthorized: User ID does not match token subject');
     }
-
     next();
   });
-});
+};
 
+
+app.use(verifyToken);
 app.use(express.json());
 
-app.get('/auth0/authMethods', authMethods);
-app.post('/auth0/updateUser', updateUser);
-app.post('/auth0/updatePassword', updatePassword);
-app.post('/auth0/enrollMFA', enrollMFA);
-app.post('/auth0/confirmMFA', confirmMFA);
-app.post('/auth0/unenrollMFA', unenrollMFA);
-app.post('/auth0/removeConnection', removeConnection);
-app.post('/auth0/challengeMfa', challengeMfa);
-app.post('/auth0/requestMFAToken', requestMFAToken);
+app.get('/auth0/authMethods', auth0.authMethods);
+app.post('/auth0/updateUser', auth0.updateUser);
+app.post('/auth0/updatePassword', auth0.updatePassword);
+app.post('/auth0/enrollMFA', auth0.enrollMFA);
+app.post('/auth0/confirmMFA', auth0.confirmMFA);
+app.post('/auth0/unenrollMFA', auth0.unenrollMFA);
+app.post('/auth0/removeConnection', auth0.removeConnection);
+app.post('/auth0/challengeMfa', auth0.challengeMfa);
+app.post('/auth0/requestMFAToken', auth0.requestMFAToken);
 
 export const auth0 = onRequest(app);
--- lib/controllers/auth0_user_api.dart
+++ lib/controllers/auth0_user_api.dart
@@ -1,11 +1,8 @@
 import 'package:angeleno_project/models/user.dart';
+
 import '../models/password_reset.dart';
-import '../utils/constants.dart';
 
 abstract class Api {
-
-  final String baseUrl = cloudFunctionURL;
-
   void updateUser(final User user);
 
   void updatePassword(final PasswordBody body);
